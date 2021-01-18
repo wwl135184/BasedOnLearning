79d382bc92309fd2572dcb7ebc93d79ba99a8708
wwl <wwl18353209005@163.com>
2021-01-18 22:49:43 +0800

    css

--- Arithmetic/interview.js
+++ Arithmetic/interview.js
@@ -43,3 +43,49 @@
     let arr6 = [...new Set([...arr2].filter(i => !arr3.has(i)))];
     console.log(arr6); // [1]
 }
+
+// 原型链
+
+// 一、原型
+
+// 原型prototype和_proto_
+// 每个对象都有一个_proto_属性，并且指向它的prototype原型对象
+// 每个构造函数都有一个prototype原型对象
+// prototype原型对象的constructor指向构造函数本身
+
+// 实例对象的_proto_指定构造函数的prototype，从而实现继承
+
+//  
+
+{
+    function Person(nick, age) {
+        this.nick = nick;
+        this.age = age;
+    }
+    Person.prototype.sayName = function() {
+        console.log(this.nick);
+        console.log(this.age);
+    }
+
+    let p1 = new Person('Byron', 20);
+    p1.sayName();
+
+    let p2 = new Person('Casper', 22);
+    p2.sayName();
+
+    console.log(p1.__proto__ === Person.prototype); // true
+    console.log(p2.__proto__ === Person.prototype); // true
+    console.log(p1.__proto__ === p2.__proto__); // true
+    console.log(Person.prototype.constructor === Person); // true
+    console.log(Object.getPrototypeOf(p1)); // Person { sayName: [Function] }
+    console.log(Object.getPrototypeOf(Person)); // [Function]
+}
+
+// Object.create(null)没有_proto_属性
+
+// 原型链
+
+{
+    let arr = [1, 2, 3];
+    console.log(arr.valueOf()); // [1, 2, 3]
+}
