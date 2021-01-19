d7a5c1be429b3cb922a97ce82e3ad86c4ae5c055
wwl <wwl18353209005@163.com>
2021-01-19 23:02:08 +0800

    exercise

--- Arithmetic/interview.js
+++ Arithmetic/interview.js
@@ -49,13 +49,12 @@
 // 一、原型
 
 // 原型prototype和_proto_
+
 // 每个对象都有一个_proto_属性，并且指向它的prototype原型对象
 // 每个构造函数都有一个prototype原型对象
 // prototype原型对象的constructor指向构造函数本身
 
 // 实例对象的_proto_指定构造函数的prototype，从而实现继承
-
-//  
 
 {
     function Person(nick, age) {
@@ -85,7 +84,80 @@
 
 // 原型链
 
+// 当试图访问一个对象的属性时，他不仅在该对象上搜寻，还会搜寻该对象的原型，以及该对象原型的原型，依次层层向上搜索，直到找到一个名字匹配的属性或达到原型链的末尾
+
+// Object.prototype._proto_ === null，意味着查找结束
+
 {
     let arr = [1, 2, 3];
     console.log(arr.valueOf()); // [1, 2, 3]
+    console.log(arr.__proto__); // []
+    console.log(Object.getPrototypeOf(arr)); // []
+    console.log(arr.__proto__.__proto__.valueOf); // [Function: valueof]
+    console.log(Array.prototype.__proto__.constructor); // [Function: Object]
 }
+
+// arr ---> arr._proto_ ---> arr._proto_._proto ---> null
+
+// javeScript中的继承
+
+// 继承是指一个对象直接使用另一个对象的属性和方法
+
+// 得到一个对象的属性
+// 得到一个对象的方法
+
+{
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    Person.prototype.getName = function() {
+        console.log(this.name);
+        console.log(this.age);
+    }
+
+    function Teacher(name, age, subject) {
+        Person.call(this, name, age);
+        this.subject = subject;
+    }
+
+    Teacher.prototype = Object.create(Person.prototype);
+    Teacher.prototype.constructor = Teacher;
+
+    let teacher = new Teacher('jack', 22, Math);
+    console.log(teacher.name); // jack
+    console.log(teacher.age); // 22
+    teacher.getName(); // jack 22
+}
+
+// hasOwnProperty
+
+// hasOwnProperty是JavaScript中唯一处理属性的且不会遍历原型链的方法。
+
+// 原型
+
+// 每个对象都有一个_proto_属性，并且指向它的prototype原型对象
+// 每个构造函数都有一个prototype原型对象
+// prototype原型对象里的constructor指向构造函数本身
+
+// 原型链
+
+// 每一个对象都有一个_proto_，它指向它的prototype原型对象，而prototype原型对象又具有一个自己的prototype原型对象，这样层层往上直到一个对象的原型prototype为null
+// 这个查询的路径就是原型链
+
+// JavaScript中的继承
+
+// 继承就是指一个对象直接使用另一个对象的属性和方法
+
+console.log('============================================================');
+
+// Vue生命周期
+
+// beforeCreate 实例完全被创建出来之前，vue实例的挂在元素$el与data数据对象都为undefined，还未初始化 
+// created 数据对象data已存在，可以调用methods中的方法，操作data中的数据，但dom未生成，$el未存在
+// beforeMount、mounted
+// beforeUpdate、updated
+// beforeDestroy、destroyed
+
+console.log('============================================================');
