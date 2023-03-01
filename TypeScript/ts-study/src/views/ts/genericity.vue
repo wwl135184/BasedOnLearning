0a73d618cfa5b059a24001848d46ca7bf6f16e51
魏文力 <weiwenli@haier.com>
2023-03-01 18:02:32 +0800

    identity函数

--- TypeScript/ts-study/src/views/ts/genericity.vue
+++ TypeScript/ts-study/src/views/ts/genericity.vue
@@ -6,8 +6,23 @@
 
 <script lang="ts" setup>
 import { ref, reactive } from "vue";
-
 // 泛型
+
+// 泛型之Hello World
+
+// identity函数
+// 不使用泛型
+{
+  function identity(arg: number): number {
+    return arg;
+  }
+}
+// 使用any类型来定义函数
+{
+  function identity(arg: any): any {
+    return arg;
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
